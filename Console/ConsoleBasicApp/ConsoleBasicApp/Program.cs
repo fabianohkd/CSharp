37aec48b62fd1d0dce68ba240422fa522581188d
fabianohkd <fabianoconrado@gmail.com>
2020-04-13 20:23:44 -0300

    Aula 01 - Primeiro projeto

---
+++ Console/ConsoleBasicApp/ConsoleBasicApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleBasicApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var opcao = Inicio(); //chama o metodo de perguntas e seta na variavel a opção selecionada
+
+            Console.Clear(); //limpa o console
+
+            switch (opcao)
+            {
+                case "1":
+                    Console.WriteLine("Opção 1 selecionada");
+                    break;
+
+                case "2":
+                    Console.WriteLine("Opção 2 selecionada");
+                    break;
+
+                case "3":
+                    Console.WriteLine("Opção 3 selecionada");
+                    break;
+
+                default:
+                    Console.WriteLine("Opção invalida!");
+                    Console.ReadKey();
+                    break;
+            }
+
+            Inicio();
+        
+        }
+
+
+        static string Inicio()
+        {
+            Console.WriteLine(
+@"Digite uma das opções para continuar:
+
+    1. Cadastro basico
+    2. Calculando sua idade
+    3. Contar uma piada
+");
+            return Console.ReadLine();
+        }
+
+
+
+    }
+}
