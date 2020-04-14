af7659d8da1cf1434c2ae9d74cff221d6b6b2b00
Fabiano Conrado <fabianoconrado@gmail.com>
2020-04-13 22:29:16 -0300

    Ajustando o codigo para a aula02

--- Console/ConsoleBasicApp/ConsoleBasicApp/Program.cs
+++ Console/ConsoleBasicApp/ConsoleBasicApp/Program.cs
@@ -7,36 +7,39 @@ namespace ConsoleBasicApp
 
         static void Main(string[] args)
         {
-            var opcao = Inicio(); //chama o metodo de perguntas e seta na variavel a opção selecionada
 
-            Console.Clear(); //limpa o console
+            ConsoleKeyInfo keyInfo;
 
-            switch (opcao)
+            do
             {
-                case "1":
-                    Console.WriteLine("Opção 1 selecionada");
-                    break;
+                keyInfo = Inicio(); //chama o metodo de perguntas e seta na variavel a opção selecionada
+                Console.Clear(); //limpa o console
 
-                case "2":
-                    Console.WriteLine("Opção 2 selecionada");
-                    break;
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.WriteLine("Opção 1 selecionada");
+                        break;
 
-                case "3":
-                    Console.WriteLine("Opção 3 selecionada");
-                    break;
+                    case ConsoleKey.D2:
+                        Console.WriteLine("Opção 2 selecionada");
+                        break;
 
-                default:
-                    Console.WriteLine("Opção invalida!");
-                    Console.ReadKey();
-                    break;
-            }
+                    case ConsoleKey.D3:
+                        Console.WriteLine("Opção 3 selecionada");
+                        break;
 
-            Inicio();
-        
+                    default:
+                        Console.WriteLine("Opção invalida!");
+                        break;
+                }
+
+            } while (keyInfo.Key != ConsoleKey.Escape);
+
         }
 
 
-        static string Inicio()
+        static ConsoleKeyInfo Inicio()
         {
             Console.WriteLine(
 @"Digite uma das opções para continuar:
@@ -45,7 +48,7 @@ namespace ConsoleBasicApp
     2. Calculando sua idade
     3. Contar uma piada
 ");
-            return Console.ReadLine();
+            return Console.ReadKey();
         }
 
 
