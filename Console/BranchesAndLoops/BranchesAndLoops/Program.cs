976117d2ecbe942367f19307887d58bb1328ee7f
Fabiano Conrado <fabianoconrado@gmail.com>
2020-04-15 23:23:33 -0300

    Incluindo a Aula 02

---
+++ Console/BranchesAndLoops/BranchesAndLoops/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BranchesAndLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://docs.microsoft.com/pt-br/dotnet/csharp/tutorials/intro-to-csharp/branches-and-loops?tutorial-step=6
+
+            int sum = 0;
+            for (int number = 1; number < 21; number++)
+            {
+                if (number % 3 == 0)
+                {
+                    sum = sum + number;
+                }
+            }
+            Console.WriteLine($"The sum is {sum}");
+        }
+    }
+}
