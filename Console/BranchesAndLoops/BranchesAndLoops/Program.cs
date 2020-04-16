7a1ee4021279d8bbc8e8e6d62112aa8244896146
Fabiano Conrado <fabianoconrado@gmail.com>
2020-04-15 23:34:37 -0300

    Incluindo enunciado

--- Console/BranchesAndLoops/BranchesAndLoops/Program.cs
+++ Console/BranchesAndLoops/BranchesAndLoops/Program.cs
@@ -8,6 +8,12 @@ namespace BranchesAndLoops
         {
             //https://docs.microsoft.com/pt-br/dotnet/csharp/tutorials/intro-to-csharp/branches-and-loops?tutorial-step=6
 
+            // Agora que você viu a instrução if e as construções de loop na linguagem C#, verifique se você pode escrever o código C# para encontrar a soma de todos os inteiros de 1 a 20 divisíveis por 3. Veja algumas dicas:
+
+            // O operador % retorna o restante de uma operação de divisão.
+            // A instrução if retorna a condição para ver se um número deve ser parte da soma.
+            // O loop for pode ajudar você a repetir uma série de etapas para todos os números de 1 a 20.
+
             int sum = 0;
             for (int number = 1; number < 21; number++)
             {
